ff323cb1082c56a997d08c53c00bf9d45f085adc
Anoop <nakkasaicharan@gmail.com>
2024-06-16 19:13:01 -0700

    test firstrest branch

--- src/main/java/com/rest/webservices/restful_web_services/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/restful_web_services/RestfulWebServicesApplication.java
@@ -9,5 +9,7 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+	
+//	test branches - first rest
 
 }
